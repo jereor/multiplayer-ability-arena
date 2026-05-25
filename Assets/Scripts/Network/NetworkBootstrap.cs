42dbc5654a86c55daccc57b12418753a3c9c6f5f
jereor <jere.orava@hotmail.com>
2026-05-25 15:05:12 +0300

    Integrate `PlayerInputHandler` into `NetworkBootstrap` and create a PlayerInputHandler object into the Bootstrap scene.

--- Assets/Scripts/Network/NetworkBootstrap.cs
+++ Assets/Scripts/Network/NetworkBootstrap.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Fusion;
+using Player;
 
 namespace Network
 {
@@ -7,11 +8,16 @@ namespace Network
     {
         [SerializeField]
         private NetworkRunner networkRunnerPrefab;
+        
+        [SerializeField]
+        private PlayerInputHandler playerInputHandler;
 
         private async void Start()
         {
             var networkRunner = Instantiate(networkRunnerPrefab);
             networkRunner.ProvideInput = true;
+            
+            networkRunner.AddCallbacks(playerInputHandler);
             
             await networkRunner.StartGame(new StartGameArgs
             {
