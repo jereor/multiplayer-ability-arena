5a283fa4ea2f660fe9076eb37fed40fbe3f08424
jereor <jere.orava@hotmail.com>
2026-05-26 20:45:22 +0300

    Replace `Rigidbody`-based movement with `NetworkCharacterController`. Add `[RequireComponent]` for dependency enforcement and initialize component in `Awake`.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 namespace Player
 {
+    [RequireComponent(typeof(NetworkCharacterController))]
     public class PlayerController : NetworkBehaviour
     {
-        [SerializeField]
-        private Rigidbody rb;
+        private NetworkCharacterController _controller;
         
         [SerializeField, Range(0f, 10f)]
         private float speed = 5f;
 
+        private void Awake()
+        {
+            _controller = GetComponent<NetworkCharacterController>();
+        }
+        
         public override void FixedUpdateNetwork()
         {
+            if (!HasInputAuthority)
+                return;
+            
             if (GetInput(out NetworkInputData input))
             {
                 var move = new Vector3(input.Move.x, 0, input.Move.y);
 
-                rb.linearVelocity = move.normalized * speed;
+                _controller.Move(move.normalized * speed * Time.fixedDeltaTime);
             }
         }
     }
